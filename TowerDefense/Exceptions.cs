d7b4be7539ceacfc15ad818d2ccc0c3c954f5c70
Nyjel Dukes <ndukes@computermentors.org>
2022-11-19 22:19:26 -0500

    Add project files.

---
+++ TowerDefense/Exceptions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TowerDefense
+{
+    class TowerDefenseExceptions : Exception
+    {
+        public TowerDefenseExceptions()
+        {
+
+        }
+
+        public TowerDefenseExceptions(string message) : base(message)
+        {
+
+        }
+    }
+
+    class OutOfBoundsException : TowerDefenseExceptions
+    {
+        public OutOfBoundsException()
+        {
+
+        }
+
+        public OutOfBoundsException(string message) : base(message)
+        {
+
+        }
+    }
+}
