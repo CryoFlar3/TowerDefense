75b87ec3c311939edc6d01ce3b7f805771a4ec29
Nyjel Dukes <ndukes@computermentors.org>
2022-12-04 20:15:07 -0500

    Towers can now miss the Invaders

--- TowerDefense/Tower.cs
+++ TowerDefense/Tower.cs
@@ -10,6 +10,7 @@ namespace TowerDefense
     {
         private const int _range = 1;
         private const int _power = 1;
+        private const double _accuracy = .75;
 
         private static readonly Random _random = new Random();
 
@@ -19,10 +20,22 @@ namespace TowerDefense
             _location = location;
         }
 
+        public bool IsSuccessfulShot() {
+            return _random.NextDouble() < _accuracy;
+        }
+
         public void FireOnInvaders(Invader[] invaders) {
             foreach (Invader invader in invaders) {
-                if(invader.IsActive && _location.InRangeOf(invader.Location, _range)) {
-                    invader.DecreaseHealth(_power);
+                if (invader.IsActive && _location.InRangeOf(invader.Location, _range)) {
+                    if (IsSuccessfulShot()) {
+                        invader.DecreaseHealth(_power);
+                        Console.WriteLine("Hit Invader");
+                        if(invader.IsNeutralized) {
+                            Console.WriteLine("Invader Neutralized");
+                        }
+                    } else {
+                        Console.WriteLine("Missed Invader");
+                    }
                     break;
                 }
             }
